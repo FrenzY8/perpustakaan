2f036d93beca04c12ebcedc62b906bb262a92046
FrenzY8 <siti12011972@gmail.com>
2026-04-07 16:57:12 +0700

    DB

--- database/JokopusDB.sql
+++ database/JokopusDB.sql
@@ -14,29 +14,210 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+-- Dumping structure for table perpustakaan.buku
+CREATE TABLE IF NOT EXISTS `buku` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `judul` varchar(255) NOT NULL,
+  `gambar_sampul` varchar(255) DEFAULT NULL,
+  `ringkasan` text NOT NULL,
+  `penerbit` varchar(255) NOT NULL,
+  `isbn` varchar(255) NOT NULL,
+  `jumlah_halaman` int(11) NOT NULL,
+  `tanggal_terbit` date NOT NULL,
+  `format` varchar(255) NOT NULL,
+  `id_penulis` bigint(20) unsigned NOT NULL,
+  `id_kategori` bigint(20) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `price` varchar(50) DEFAULT NULL,
+  `size` varchar(50) DEFAULT NULL,
+  `rating` float DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `buku_isbn_unique` (`isbn`),
+  KEY `buku_id_penulis_foreign` (`id_penulis`),
+  KEY `buku_id_kategori_foreign` (`id_kategori`),
+  CONSTRAINT `buku_id_kategori_foreign` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `buku_id_penulis_foreign` FOREIGN KEY (`id_penulis`) REFERENCES `penulis` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table perpustakaan.buku_favorit_user
+CREATE TABLE IF NOT EXISTS `buku_favorit_user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` bigint(20) unsigned NOT NULL,
+  `id_buku` bigint(20) unsigned NOT NULL,
+  `dibuat_pada` timestamp NULL DEFAULT NULL,
+  `diperbarui_pada` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `buku_favorit_user_id_user_foreign` (`id_user`),
+  KEY `buku_favorit_user_id_buku_foreign` (`id_buku`),
+  CONSTRAINT `buku_favorit_user_id_buku_foreign` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `buku_favorit_user_id_user_foreign` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table perpustakaan.buku_tag
+CREATE TABLE IF NOT EXISTS `buku_tag` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id_buku` bigint(20) unsigned NOT NULL,
+  `id_tag` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `buku_tag_id_buku_foreign` (`id_buku`),
+  KEY `buku_tag_id_tag_foreign` (`id_tag`),
+  CONSTRAINT `buku_tag_id_buku_foreign` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `buku_tag_id_tag_foreign` FOREIGN KEY (`id_tag`) REFERENCES `tag` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=408 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table perpustakaan.kategori
+CREATE TABLE IF NOT EXISTS `kategori` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_kategori` (`nama`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table perpustakaan.komentar_buku
+CREATE TABLE IF NOT EXISTS `komentar_buku` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` bigint(20) unsigned NOT NULL,
+  `id_buku` bigint(20) unsigned NOT NULL,
+  `isi_komentar` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `komentar_buku_id_user_foreign` (`id_user`),
+  KEY `komentar_buku_id_buku_foreign` (`id_buku`),
+  CONSTRAINT `komentar_buku_id_buku_foreign` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `komentar_buku_id_user_foreign` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table perpustakaan.messages
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `sender_id` bigint(20) unsigned NOT NULL,
+  `receiver_id` bigint(20) unsigned NOT NULL,
+  `message` text NOT NULL,
+  `is_read` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `messages_receiver_id_foreign` (`receiver_id`),
+  KEY `chat_pairing` (`sender_id`,`receiver_id`),
+  CONSTRAINT `messages_receiver_id_foreign` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `messages_sender_id_foreign` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
+
+-- Dumping structure for table perpustakaan.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table perpustakaan.peminjaman
+CREATE TABLE IF NOT EXISTS `peminjaman` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` bigint(20) unsigned NOT NULL,
+  `id_buku` bigint(20) unsigned NOT NULL,
+  `id_admin` bigint(20) unsigned DEFAULT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `tanggal_jatuh_tempo` date NOT NULL,
+  `tanggal_kembali` date DEFAULT NULL,
+  `status` enum('menunggu','dipinjam','dikembalikan','terlambat','ditolak') NOT NULL DEFAULT 'menunggu',
+  `catatan_admin` text DEFAULT NULL,
+  `dibuat_pada` timestamp NULL DEFAULT NULL,
+  `diperbarui_pada` timestamp NULL DEFAULT NULL,
+  `potongan_denda` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `peminjaman_id_user_foreign` (`id_user`),
+  KEY `peminjaman_id_buku_foreign` (`id_buku`),
+  KEY `peminjaman_id_admin_foreign` (`id_admin`),
+  CONSTRAINT `peminjaman_id_admin_foreign` FOREIGN KEY (`id_admin`) REFERENCES `users` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `peminjaman_id_buku_foreign` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `peminjaman_id_user_foreign` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table perpustakaan.penulis
+CREATE TABLE IF NOT EXISTS `penulis` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_penulis` (`nama`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table perpustakaan.personal_access_tokens
+CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table perpustakaan.ratings
+CREATE TABLE IF NOT EXISTS `ratings` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `buku_id` bigint(20) unsigned NOT NULL,
+  `skor` tinyint(3) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_buku_unique` (`user_id`,`buku_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table perpustakaan.tag
+CREATE TABLE IF NOT EXISTS `tag` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_tag` (`nama`)
+) ENGINE=InnoDB AUTO_INCREMENT=408 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table perpustakaan.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_photo` varchar(255) DEFAULT NULL,
+  `role` varchar(255) NOT NULL DEFAULT 'user',
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
